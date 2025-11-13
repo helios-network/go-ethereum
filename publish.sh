56a39829e4eacc58af0729d5a6abbab51daa064e
jguyet <jguyet.weoohh@gmail.com>
2025-11-13 14:50:08 +0100

    Publish v1.10.26-rc4

---
+++ publish.sh
@@ -0,0 +1,11 @@
+VERSION=${VERSION:-"v1.10.26-rc4"}
+
+echo "Deploy go-ethereum"
+git add .
+git commit -m "Publish $VERSION"
+git push
+git tag $VERSION
+git push origin $VERSION
+GOPROXY=proxy.golang.org go list -m github.com/Helios-Chain-Labs/go-ethereum@$VERSION
+
+echo "Publish done"
